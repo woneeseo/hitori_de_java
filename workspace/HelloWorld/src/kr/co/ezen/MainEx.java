ca87249fec639dae8fe0bb9bdbdc0a41dbfefe59
yewon <hon0760@naver.com>
2020-10-15 23:21:16 +0900

    append a workspace for github

---
+++ workspace/HelloWorld/src/kr/co/ezen/MainEx.java
@@ -0,0 +1,13 @@
+package kr.co.ezen;
+
+public class MainEx {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("today's lecture");
+		System.out.println("learned sys");
+		System.out.println("and create java projects");
+		}
+
+}
